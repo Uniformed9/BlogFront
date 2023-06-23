ca04ef4b2e9fe1fcecc1b4ed676e51113333a3d6
hjc <2269081458@qq.com>
2023-06-23 20:49:28 +0800

    前端Comment组件

--- src/components/componet/SearchPage.vue
+++ src/components/componet/SearchPage.vue
@@ -1,9 +1,7 @@
 <template class="all">
   <div class="body">
     <div class="videoContainer">
-      <video class="fullscreenVideo" id="bgVid" playsinline="" autoplay="" muted="" loop="">
-        <source src="@/assets/hutao.mp4" type="video/mp4">
-      </video>
+
     </div>
     <NavBar></NavBar>
     <div class="inputBox">
